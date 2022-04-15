fd529f65a5fcb99e20d776cb2553aecf91d3fb3f
Arman-Aslanyan <67847062+Arman-Aslanyan@users.noreply.github.com>
2022-04-15 14:59:27 -0700

    Grass + Water | better random chance

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,58 +5,64 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    GameManager INSTANCE;
     PlayerController player;
     public Resumon[] resumon = new Resumon[3];
     public GameObject prefab;
-    float time = 2.5f;
+    float time = 1;
 
-    private void Awake()
+    void Awake()
     {
         DontDestroyOnLoad(gameObject);
+        INSTANCE = this;
     }
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerController>();
+        GameManager[] objs = FindObjectsOfType<GameManager>();
+        if (objs.Length > 1)
+            for (int i = 1; i < objs.Length; i++)
+                Destroy(objs[i].gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+
     }
 
     //Called once per physics frame
     private void FixedUpdate()
     {
-        player.isGrassed = player.GetGrassed();
         if (player.isGrassed && time >= 2.5f)
         {
             time = 0;
             Encounter();
         }
-        else
+        else if (time < 100)
             time += Time.fixedDeltaTime;
     }
 
     public void Encounter()
     {
         SceneManager.LoadScene("Combat");
-        int randResuNum;
         float rand = Random.value;
-        if (rand < 0.4)
-            randResuNum = 0;
-        else if (rand <= 7)
-            randResuNum = 1;
-        else randResuNum = 2;
-        RenderResumon(resumon[randResuNum], Vector3.zero);
-        print(resumon[randResuNum] + " | " + randResuNum);
+        int randNum;
+        if (rand <= 0.65f) randNum = 0;
+        else if (rand <= 0.85f) randNum = 1;
+        else randNum = 2;
+        player.isGrassed = false;
+        RenderResumon(resumon[randNum], Vector3.zero);
+        print(resumon[randNum] + " | " + randNum);
     }
 
     public void RenderResumon(Resumon toRender, Vector3 pos)
     {
-        GameObject obj = Instantiate(prefab, pos, Quaternion.identity);
+        GameObject obj = transform.GetChild(0).gameObject;
+        obj.transform.position = pos;
         obj.GetComponent<SpriteRenderer>().sprite = toRender.sprite;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,16 @@ using UnityEngine;
 [System.Serializable]
 public class grassDetector
 {
-    public Vector3 pos;
-    public Vector3 scale;
     public LayerMask mask;
 
-    public grassDetector(Vector3 pos, Vector3 scale, LayerMask mask)
+    public grassDetector(LayerMask mask)
         : this()
     {
-        this.pos = pos;
-        this.scale = scale;
         this.mask = mask;
     }
 
     public grassDetector()
     {
-        pos = Vector3.zero;
-        scale = Vector3.one;
         mask = 4160;
     }
 }
@@ -44,23 +38,15 @@ public class PlayerVals : MonoBehaviour
 
 public class PlayerController : MonoBehaviour
 {
-    [HideInInspector]
-    public Rigidbody2D rb;
-    [HideInInspector]
-    public Collider2D coll;
-    [HideInInspector]
-    public bool isGrassed;
+    [HideInInspector] public Rigidbody2D rb;
+    [HideInInspector] public Collider2D coll;
+    [HideInInspector] public bool isGrassed;
 
     public grassDetector gCheck = new grassDetector();
     public float moveSpeed = 5f;
     private Animator animator;
     Vector2 movement;
     public bool canMove = true;
-
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -87,17 +73,16 @@ public class PlayerController : MonoBehaviour
             rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 
-    public bool GetGrassed()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        return Physics2D.OverlapBox(gCheck.pos + transform.position, gCheck.scale, 0, gCheck.mask);
+        if (other.CompareTag("Grass"))
+        {
+            isGrassed = true;
+        }
     }
 
-    private void OnDrawGizmosSelected()
+    private void OnTriggerExit2D(Collider2D other)
     {
-        if (isGrassed)
-            Gizmos.color = Color.red;
-        else
-            Gizmos.color = Color.white;
-        Gizmos.DrawCube(gCheck.pos + transform.position, gCheck.scale);
+        isGrassed = false;
     }
 }
